eb8ac6489f8e9a6f3e6f959f4e692db8576550c1
Víctor Córdova <theslapboy@gmail.com>
2019-05-30 17:38:51 -0700

    From send email

--- FormSendEmail/BajadevMail.cs
+++ FormSendEmail/BajadevMail.cs
@@ -8,21 +8,7 @@ using System.Web.UI;
 using System.Xml;
 
 namespace FormSendEmail
-{
-    public class Mail
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
-        public string DoNotCall { get; set; }
-        public string Industry { get; set; }
-        public string LeadSource { get; set; }
-        public string Description { get; set; }
-    }
-
+{   
     public class BajadevMail : Page
     {
 
---
+++ FormSendEmail/Mail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormSendEmail
+{
+    public class Mail
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string DoNotCall { get; set; }
+        public string Industry { get; set; }
+        public string LeadSource { get; set; }
+        public string Description { get; set; }
+    }
+}
